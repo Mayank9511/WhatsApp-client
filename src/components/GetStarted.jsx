8c7a13c50cf8900bdbdaa229ac38e90c8e120d75
Mayank9511 <gupta.mayank.mg01@gmail.com>
2024-09-03 02:28:02 +0530

    tunnel

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import { CircularProgress, Snackbar, Box, Alert } from "@mui/material";
 import { motion } from "framer-motion";
 
-const socket = io("https://finances-starts-frames-whenever.trycloudflare.com");
+const socket = io("https://baseball-normal-inflation-senegal.trycloudflare.com");
 // const socket = io("http://localhost:5001");
 
 function GetStarted() {
