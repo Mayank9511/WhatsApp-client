d31617de00203389168321d51c67b1de1b392e54
Mayank9511 <gupta.mayank.mg01@gmail.com>
2024-11-06 13:37:29 +0530

    using tunnel by env

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,21 +2,21 @@ import React, { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
 import { io } from "socket.io-client";
 import { CircularProgress, Snackbar, Box, Alert } from "@mui/material";
+import { motion } from "framer-motion";
 
-const socket = io("http://localhost:5000/");
+const tunnel = import.meta.env.VITE_TUNNEL;
+const socket = io(tunnel);
 
 function GetStarted() {
   const [prompt, setPrompt] = useState("");
   const [qrImage, setQrImage] = useState(null);
   const [loader, setLoader] = useState(false);
-  const [alertOpen, setAlertOpen] = useState(false);
   const [linked, setLinked] = useState(false);
-
+  const [isAnimating, setIsAnimating] = useState(false);
   const [socketId, setSocketId] = useState("");
-  
+
   socket.on("hello", (id) => {
     setSocketId(id);
-    // console.log("hello: ", id);
 
     try {
       setLoader(true);
@@ -27,7 +27,7 @@ function GetStarted() {
       });
     } catch (error) {
       setLoader(false);
-      // console.error("Error fetching QR image:", error);
+      alert("Some issue occurred, please try again or contact the owner.")
     }
   });
 
@@ -35,46 +35,37 @@ function GetStarted() {
     setLinked(true);
   });
 
-  // const canGenerateQR = () => {
-  //   return qrImage !== null || loader !== false;
-  // };
-
   const handleSubmit = async () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 1500);
     try {
       socket.emit("prompt", { prompt, socketId });
     } catch (error) {
-      console.error("Error generating image:", error);
+      console.error("Error:", error);
+      alert("Some issue occurred, please try again or contact the owner.")
     }
   };
 
-  // const handleQR = () => {
-  //   try {
-  //     setLoader(true);
-  //     socket.emit("QR", socketId);
-  //     socket.on("QRcode", (base64Qr) => {
-  //       setQrImage(base64Qr);
-  //       setLoader(false);
-  //     });
-  //   } catch (error) {
-  //     setLoader(false);
-  //     console.error("Error fetching QR image:", error);
-  //   }
-  // };
+  const bounceAnimation = {
+    animate: {
+      x: [0, 2, 4, 6, 8, 10, 12, 0],
+      y: [0, -15, 0, -10, 0, -5, 0, 0],
+      transition: {
+        duration: 1.5,
+        times: [0, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 1],
+      },
+    },
+  };
 
   useEffect(() => {
     socket.on("connect", () => {
       // console.log("socket is connected: ", socket.id);
     });
 
-    socket.on("disconnect",socket.id);
-    
+    socket.on("disconnect", socket.id);
   }, []);
-
-  // useEffect(() => {
-  //   console.log("Loader state changed:", loader);
-  //   // if (loader) console.log("is loading:");
-  //   // else console.log("loading is completed:");
-  // }, [loader]);
 
   return (
     <div
@@ -117,42 +108,35 @@ function GetStarted() {
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt"
-            className="w-full md:w-3/4 h-[10vw] md:h-[5vw] p-[2vw] md:p-[1.5vw] mt-[4vw] md:mt-[2vw] rounded-xl"
+            className="w-full md:w-3/4 h-[10vw] md:h-[5vw] p-[2vw] md:p-[1.5vw] mt-[20px] rounded-xl"
           />
           <button
             onClick={handleSubmit}
-            className="flex uppercase gap-4 md:gap-10 items-center px-6 md:px-10 py-4 md:py-6 bg-zinc-900 rounded-full mt-6 md:mt-10 text-white"
+            className="flex uppercase gap-1 md:gap-1 items-center px-6 md:px-10 py-4 md:py-6 bg-zinc-900 rounded-full mt-[20px] text-white"
           >
             Set Prompt
-            <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+            <motion.div
+              animate={isAnimating ? bounceAnimation.animate : {}}
+              className="w-[4px] h-[4px] mt-[12px] bg-zinc-100 rounded-full"
+            ></motion.div>
           </button>
         </div>
         <div className="w-full md:w-1/2 flex justify-center">
-          <div className="w-[320px] h-[320px] m-[5vw] md:ml-[4vw] rounded-3xl bg-[#fff]">
+          <div className="min-w-[320px] h-[320px] m-[5vw] flex justify-center items-center md:ml-[4vw] rounded-3xl bg-[#fff]">
             {loader && (
-              <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+              <Box>
                 <CircularProgress />
               </Box>
             )}
 
             {qrImage && !loader && (
-              <div className="qr-container w-[320px] h-[320px] flex justify-center items-center">
+              <div className="w-[320px] h-[320px] flex justify-center items-center">
                 <QRCode className="qr-code" value={qrImage} size={280} />
               </div>
             )}
           </div>
         </div>
       </div>
-      <Snackbar
-        open={alertOpen}
-        autoHideDuration={5000}
-        onClose={() => setAlertOpen(false)}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-      >
-        <Alert severity="success" sx={{ width: "300px" }}>
-          This is an info Alert.
-        </Alert>
-      </Snackbar>
 
       <Snackbar
         open={linked}
