4d79b063e560ed853bda5b01aa4f676db38a1d1b
Mayank9511 <gupta.mayank.mg01@gmail.com>
2024-11-06 17:02:50 +0530

    tunnel and logo

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,8 +4,7 @@ import { io } from "socket.io-client";
 import { CircularProgress, Snackbar, Box, Alert } from "@mui/material";
 import { motion } from "framer-motion";
 
-const tunnel = import.meta.env.VITE_TUNNEL;
-const socket = io(tunnel);
+const socket = io("https://apollo.serveo.net/");
 
 function GetStarted() {
   const [prompt, setPrompt] = useState("");
