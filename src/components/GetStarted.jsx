d74974f6dec3ed6fda2319739bb9dc47af7f3ed4
Mayank9511 <gupta.mayank.mg01@gmail.com>
2024-11-13 15:06:37 +0530

    tunnel

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import { CircularProgress, Snackbar, Box, Alert } from "@mui/material";
 import { motion } from "framer-motion";
 
-const socket = io("https://publicorum.serveo.net");
+const socket = io("https://maximus.serveo.net");
 
 function GetStarted() {
   const [prompt, setPrompt] = useState("");
