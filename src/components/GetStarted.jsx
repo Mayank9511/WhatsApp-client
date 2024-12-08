92ed6c6c6d4f662869750f8e13abf997f0543172
Mayank9511 <gupta.mayank.mg01@gmail.com>
2024-12-08 11:25:07 +0530

    tunnel

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import { CircularProgress, Snackbar, Box, Alert } from "@mui/material";
 import { motion } from "framer-motion";
 
-const socket = io("https://owns-hourly-lf-using.trycloudflare.com");
+const socket = io("https://epic-prisoners-geneva-rebate.trycloudflare.com");
 
 
 function GetStarted() {
